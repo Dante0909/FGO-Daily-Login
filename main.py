7412ed4646afcbd0d880cf9a8cbb1aa8ab0decdc
Dante0909 <60408356+Dante0909@users.noreply.github.com>
2023-03-09 09:19:03 -0500

    changed hour 10 to 9

--- main.py
+++ main.py
@@ -57,8 +57,7 @@ def main():
                 instance.topHome()
                 time.sleep(2)
                 hour = datetime.datetime.utcnow().time().hour
-                logger.info(hour)
-                if hour == 10:
+                if hour == 9:
                     logger.info('Throw daily friend summon!')
                     instance.drawFP()
                     time.sleep(2)
